eddea697f37ab5971c4fb5d4c6985f931a048903
Rafael Eyng <rafaeleyng@gmail.com>
2016-09-07 01:07:17 -0300

    remove game logic from app and use "lif" solver

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 import _ from 'lodash'
+import lif from 'lif'
 import Cell from './Cell'
 
 class App extends Component {
@@ -9,7 +10,14 @@ class App extends Component {
 
     const width = 16
     const height = 16
-    const board = this.initEmptyBoard(width, height)
+
+    const initEmptyBoard = (width, height) => {
+      const board = []
+      for (var i = 0; i < width * height; ++i) { board[i] = false }
+      return board
+    }
+
+    const board = initEmptyBoard(width, height)
 
     this.state = {
       interval: 100,
@@ -20,129 +28,14 @@ class App extends Component {
     }
 
     // bind
-    this.initEmptyBoard = this.initEmptyBoard.bind(this)
     this.applyRules = this.applyRules.bind(this)
-    this.findNeighbors = this.findNeighbors.bind(this)
-    this.wrapRow = this.wrapRow.bind(this)
     this.play = this.play.bind(this)
     this.stop = this.stop.bind(this)
     this.handleCellClick = this.handleCellClick.bind(this)
   }
 
-  initEmptyBoard(width, height) {
-    const board = []
-    for (var i = 0; i < width * height; ++i) { board[i] = false }
-    return board
-  }
-
-  wrapRow(value) {
-    const m = this.state.width * this.state.height
-    return (value + m) % (this.state.width * this.state.height)
-  }
-
-  findNeighbor1(index) {
-    const isLeftmost = index % this.state.width === 0
-    let compensation = 0
-    if (isLeftmost) {
-      compensation = this.state.width
-    }
-
-    const value = index - this.state.width - 1
-    return this.wrapRow(value + compensation)
-  }
-
-  findNeighbor2(index) {
-    const value = index - this.state.width
-    return this.wrapRow(value)
-  }
-
-  findNeighbor3(index) {
-    const isRightmost = index % this.state.width === (this.state.width - 1)
-    let compensation = 0
-    if (isRightmost) {
-      compensation = this.state.width
-    }
-
-    const value = index - this.state.width + 1
-    return this.wrapRow(value - compensation)
-  }
-
-  findNeighbor4(index) {
-    const isLeftmost = index % this.state.width === 0
-    let compensation = 0
-    if (isLeftmost) {
-      compensation = this.state.width
-    }
-    return index - 1 + compensation
-  }
-
-  findNeighbor5(index) {
-    const isRightmost = index % this.state.width === (this.state.width - 1)
-    let compensation = 0
-    if (isRightmost) {
-      compensation = this.state.width
-    }
-    return index + 1 - compensation
-  }
-
-  findNeighbor6(index) {
-    const isLeftmost = index % this.state.width === 0
-    let compensation = 0
-    if (isLeftmost) {
-      compensation = this.state.width
-    }
-
-    const value = index + this.state.width - 1
-    return this.wrapRow(value + compensation)
-  }
-
-  findNeighbor7(index) {
-    const value = index + this.state.width
-    return this.wrapRow(value)
-  }
-
-  findNeighbor8(index) {
-    const isRightmost = index % this.state.width === (this.state.width - 1)
-    let compensation = 0
-    if (isRightmost) {
-      compensation = this.state.width
-    }
-    const value = index + this.state.width + 1
-    return this.wrapRow(value - compensation)
-  }
-
-  findNeighbors(index) {
-    return [
-      this.findNeighbor1(index),
-      this.findNeighbor2(index),
-      this.findNeighbor3(index),
-      this.findNeighbor4(index),
-      this.findNeighbor5(index),
-      this.findNeighbor6(index),
-      this.findNeighbor7(index),
-      this.findNeighbor8(index),
-    ]
-  }
-
   applyRules() {
-    const board = this.initEmptyBoard(this.state.width, this.state.height)
-    this.state.board.forEach((isAlive, i) => {
-      const neighborIndexes = this.findNeighbors(i)
-      const neighborStatuses = neighborIndexes.map(ni => this.state.board[ni])
-      const aliveNeighbors = neighborStatuses.filter(e => e).length
-
-      // rules
-      if (!isAlive && aliveNeighbors === 3) {
-        board[i] = true
-      } else if (isAlive && (aliveNeighbors === 2 || aliveNeighbors === 3)) {
-        board[i] = true
-      } else if (isAlive && aliveNeighbors < 2) {
-        board[i] = false
-      } else if (isAlive && aliveNeighbors > 3) {
-        board[i] = false
-      }
-    })
-    this.setState({board})
+    this.setState({ board: lif(this.state.board) })
   }
 
   play() {
